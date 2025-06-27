b65f67bc44a63ec9638ca5dd164f9f5286187f82
Masensen <masensen.dev@proton.me>
2025-06-26 20:43:29 -0400

    +_+

--- config/install/steps.sh
+++ config/install/steps.sh
@@ -6,22 +6,22 @@ require_file "$projectDir/tasks/install/Formating_boot_partition.sh"
 
 require_file "$projectDir/tasks/install/Formating_linux_system_partition.sh"
 require_file "$projectDir/tasks/install/Formating_root.sh"
-require_file "$projectDir/"
 
 
 
-require_file "$projectDir/bash/install/Making_swap.sh"
-require_file "$projectDir/bash/install/Mirror_configuration.sh"
-require_file "$projectDir/bash/install/Base_system_setup.sh"
-require_file "$projectDir/bash/install/Configure_bootloader.sh"
-require_file "$projectDir/bash/install/Setup_users.sh"
-require_file "$projectDir/bash/install/Setup_network.sh"
-require_file "$projectDir/bash/install/Setup_window_managers.sh"
-require_file "$projectDir/bash/install/Enable_services.sh"
-require_file "$projectDir/bash/install/CodexBox_setup.sh"
-require_file "$projectDir/bash/install/Optional_package.sh"
-require_file "$projectDir/bash/install/Flatpack_setup.sh"
-require_file "$projectDir/bash/install/Snap_setup.sh"
+
+# require_file "$projectDir/bash/install/Making_swap.sh"
+# require_file "$projectDir/bash/install/Mirror_configuration.sh"
+# require_file "$projectDir/bash/install/Base_system_setup.sh"
+# require_file "$projectDir/bash/install/Configure_bootloader.sh"
+# require_file "$projectDir/bash/install/Setup_users.sh"
+# require_file "$projectDir/bash/install/Setup_network.sh"
+# require_file "$projectDir/bash/install/Setup_window_managers.sh"
+# require_file "$projectDir/bash/install/Enable_services.sh"
+# require_file "$projectDir/bash/install/CodexBox_setup.sh"
+# require_file "$projectDir/bash/install/Optional_package.sh"
+# require_file "$projectDir/bash/install/Flatpack_setup.sh"
+# require_file "$projectDir/bash/install/Snap_setup.sh"
 
 
-require_file "$projectDir/"
+# require_file "$projectDir/"
