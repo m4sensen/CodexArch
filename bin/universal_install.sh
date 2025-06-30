5a3540620bbbe1d2d85bc03ca5e290047ce2ad09
Masensen <masensen.dev@proton.me>
2025-06-30 17:27:38 -0400

    +_+

--- bin/universal_install.sh
+++ bin/universal_install.sh
@@ -1,4 +1,4 @@
-require_file "$PROJECT_DIR/tasks/universal-install/variables.sh"
+
 require_file "$PROJECT_DIR/tasks/universal-install/time.sh"
 require_file "$PROJECT_DIR/tasks/universal-install/locale.sh"
 require_file "$PROJECT_DIR/tasks/universal-install/host.sh"
